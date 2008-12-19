159c0486e68c76c1f517c61650515a86e76dec61
Marcio Faustino <marciof@home>
2008-12-19 15:43:35 +0000

    - Removed information bar. - Re-added plug-in load error message as a list item. - Reorganized source code. - Updated notes. - Other minor changes.

--- IM/Code/Behavior.js
+++ IM/Code/Behavior.js
@@ -1,45 +1,12 @@
-var _messages = {
-    en: {
-        PLUGIN_ERROR: 'Plug-in load error.',
-        PLUGIN_INIT: 'Initializing plug-in...',
-        SERVER_LABEL: 'Server:'
-    }
-};
-
-var _locale, _plugin;
-var _serverLabel, _serverStatus;
+var _plugin;
 
 
 function initialize() {
-    var language = navigator.language;
-    
-    if (!(navigator.language in _messages)) {
-        language = 'en';
-    }
-    
-    _locale = _messages[language];
     _plugin = document.getElementById('Server');
-    _serverLabel = document.getElementById('ServerLabel');
-    _serverStatus = document.getElementById('ServerStatus');
     
-    setText(_serverLabel, _locale.SERVER_LABEL);
-    
-    if ('version' in _plugin) {
-        setText(_serverStatus, _locale.PLUGIN_INIT);
-    }
-    else {
-        setText(_serverStatus, _locale.PLUGIN_ERROR);
+    if (!('version' in _plugin)) {
         return;
     }
-}
-
-
-function setText(node, text) {
-    while (_serverStatus.childNodes.length > 0) {
-        node.removeChild(node.childNodes.item(0));
-    }
-    
-    node.appendChild(document.createTextNode(text));
 }
 
 
