ed02ff65cb1ee7ef8460bd25db688ac00dbfc1e9
Marcio Faustino <marciof@home>
2009-02-10 22:53:06 +0000

    - Changed the file type to Windows Script File.

--- Attic/CreateProcess.js
+++
@@ -1,9 +0,0 @@
-var shell = new ActiveXObject('WScript.Shell');
-var fileSystem = new ActiveXObject('Scripting.FileSystemObject');
-var ForReading = 1;
-var listFileName = fileSystem.GetBaseName(WScript.ScriptFullName) + '.txt';
-var listFile = fileSystem.OpenTextFile(listFileName, ForReading, true);
-
-while (!listFile.AtEndOfStream) {
-    shell.Run(listFile.ReadLine());
-}
