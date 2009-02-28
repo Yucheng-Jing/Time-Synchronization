de4d62d695bb35972a2bf03a24626d78ba385c1c
Marcio Faustino <marciof@home>
2009-02-28 13:21:09 +0000

    - Added version string. - Clean up of the resource script file.

--- Attic/Messaging/Code/npMessaging.h
+++ Attic/Messaging/Code/npMessaging.h
@@ -3,13 +3,22 @@
 
 
 /** Major version number (year). */
-#define NPMESSAGING_MAJOR_VERSION (2009)
+#define MESSAGING_MAJOR_VERSION 2009
 
 /** Minor version number (month). */
-#define NPMESSAGING_MINOR_VERSION (02)
+#define MESSAGING_MINOR_VERSION 02
 
 /** Micro version number (day). */
-#define NPMESSAGING_MICRO_VERSION (28)
+#define MESSAGING_MICRO_VERSION 28
+
+/** Full version string. */
+#define MESSAGING_VERSION \
+    MESSAGING_VERSION_IMPLEMENTATION(MESSAGING_MAJOR_VERSION, MESSAGING_MINOR_VERSION, MESSAGING_MICRO_VERSION)
+
+#define MESSAGING_VERSION_IMPLEMENTATION(major, minor, micro) \
+    MESSAGING_VERSION_TO_STRING(major) "-" MESSAGING_VERSION_TO_STRING(minor) "-" MESSAGING_VERSION_TO_STRING(micro)
+
+#define MESSAGING_VERSION_TO_STRING(version) #version
 
 
 #endif
