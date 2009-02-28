869f0900d06bbfbb6ebbaf68a145e0c5644e57ea
Marcio Faustino <marciof@home>
2009-02-28 13:01:34 +0000

    - Added plug-in version macros. - Clean up of the resource script file.

---
+++ Attic/Messaging/Code/npMessaging.h
@@ -0,0 +1,15 @@
+#ifndef __NPMESSAGING__
+#define __NPMESSAGING__
+
+
+/** Major version number (year). */
+#define NPMESSAGING_MAJOR_VERSION (2009)
+
+/** Minor version number (month). */
+#define NPMESSAGING_MINOR_VERSION (02)
+
+/** Micro version number (day). */
+#define NPMESSAGING_MICRO_VERSION (28)
+
+
+#endif
