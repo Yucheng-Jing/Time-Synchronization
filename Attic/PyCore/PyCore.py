d02f799b23da919d91c9792448638cd4843dc024
Marcio Faustino <marciof@home>
2009-03-14 14:28:58 +0000

    - Major clean up: removed unnecessary definitions, empty classes, etc. - Added method to remove a path, be it a file or a directory tree.

--- Attic/PyCore/PyCore.py
+++ Attic/PyCore/PyCore.py
@@ -1,132 +1,124 @@
 # -*- coding: utf-8 -*-
 
 
-import abc, fnmatch, os, types, zipfile
+import fnmatch, functools, glob, math, os, shutil, types, zipfile
 
 
-abstract = abc.abstractmethod
-AbstractType = abc.ABCMeta
-
-NaN = float('NaN')
-Infinity = float('inf')
-
-Boolean = types.BooleanType
-CharSequence = basestring
-Float = types.FloatType
-Function = types.FunctionType
-Integer = types.IntType
-List = types.ListType
-Map = types.DictionaryType
-Object = types.ObjectType
-Set = set
-String = types.StringType
-Tuple = types.TupleType
-Type = types.TypeType
+_NaN = float('NaN')
+_Infinity = float('inf')
 
 
-class Path:
-    HOME = os.path.expanduser('~')
+class _ZipFile (zipfile.ZipFile, object):
+    def __init__(self, *args, **kargs):
+        super(_ZipFile, self).__init__(*args, **kargs)
     
     
-    @staticmethod
-    def create(path):
-        if not os.path.exists(path):
-            os.makedirs(path)
-    
-    
-    @staticmethod
-    def glob(filter, root = os.path.curdir, levels = Infinity):
-        """
-        Flexible recursive version of the standard
-        U{glob<http://docs.python.org/library/glob.html#glob.glob>} function.
-        
-        @type filter: Function, CharSequence
-        @param filter: a function that checks whether a path should be included
-               or a string describing the Unix-style glob pattern
-        @type root: CharSequence
-        @param root: directory in which to start searching
-        @type levels: Integer
-        @param levels: maximum number of nested directories to search
-        @rtype: List
-        @return: list with all collected paths
-        """
-        
-        names = []
-        
-        if isinstance(filter, CharSequence):
-            pattern = filter
-            filter = lambda name: fnmatch.fnmatch(name, pattern)
-        
-        if levels > 0:
-            for name in os.listdir(root):
-                path = os.path.join(root, name)
-                
-                if os.path.isdir(path):
-                    names.extend(Path.glob(filter, path, levels - 1))
-                if filter(path):
-                    names.append(path)
-        
-        return names
-
-
-class Sequence:
-    @staticmethod
-    def flatten(sequence):
-        """
-        Removes all nested sequences.
-        
-        @type sequence: List, Set, Tuple
-        @param sequence: sequence for which to remove sub-sequences
-        @rtype: List, Set, Tuple
-        @return: one-dimensional version of the given sequence
-        """
-        
-        values = []
-        
-        for element in sequence:
-            if isinstance(element, (List, Set, Tuple)):
-                values.extend(Sequence.flatten(element))
-            else:
-                values.append(element)
-        
-        return type(sequence)(values)
-    
-    
-    @staticmethod
-    def join(sequence, separator = ''):
-        """
-        Concatenates a sequence.
-        
-        @type sequence: List, Set, Tuple
-        @param sequence: sequence with the values to concatenate
-        @type separator: CharSequence
-        @param separator: string to use as the separator
-        @rtype: CharSequence
-        @return: string formed by joining all sequence elements
-        """
-        
-        return separator.join(map(String, sequence))
-
-
-class Zip:
-    @staticmethod
-    def extract_all(file, path = os.path.curdir, members = None, password = None):
-        zip = file
-        
-        if isinstance(file, CharSequence):
-            zip = zipfile.ZipFile(file, 'r')
-        
+    def extractall(self, path = os.path.curdir, members = None, password = None):
         if members is None:
-            members = zip.namelist()
+            members = self.namelist()
         
-        # Place directories first to create them before extracting files.
+        # Place directories first to create them before extracting any file.
         members.sort()
         
         for name in members:
             if name.endswith('/'):
-                Path.create(os.path.join(path, name))
+                _make_path(os.path.join(path, name))
             else:
-                zip.extract(name, path, password)
-        
-        if isinstance(file, CharSequence):
-            zip.close()
+                self.extract(name, path, password)
+
+
+def _flatten(sequence):
+    """
+    Removes all nested sequences.
+    
+    @type sequence: list, set, tuple
+    @param sequence: sequence for which to remove sub-sequences
+    @rtype: list, set, tuple
+    @return: one-dimensional version of the given sequence
+    """
+    
+    values = []
+    
+    for element in sequence:
+        if isinstance(element, (list, set, tuple)):
+            values.extend(_flatten(element))
+        else:
+            values.append(element)
+    
+    return type(sequence)(values)
+
+
+def _glob(filter, root = os.path.curdir, levels = _Infinity):
+    """
+    Recursive version of the standard
+    U{glob.glob<http://docs.python.org/library/glob.html#glob.glob>} function.
+    
+    @type filter: basestring, function
+    @param filter: a function that checks whether a path should be included
+           or a string describing the glob pattern
+    @type root: basestring
+    @param root: directory in which to start searching
+    @type levels: int
+    @param levels: maximum number of nested directories to search
+    @rtype: list
+    @return: list with all collected paths
+    """
+    
+    names = []
+    
+    if isinstance(filter, basestring):
+        pattern = filter
+        filter = lambda name: fnmatch.fnmatch(name, pattern)
+    
+    if levels > 0:
+        for name in os.listdir(root):
+            path = os.path.join(root, name)
+            
+            if os.path.isdir(path):
+                names.extend(_glob(filter, path, levels - 1))
+            if filter(path):
+                names.append(path)
+    
+    return names
+
+
+def _make_path(path):
+    """
+    Recursively creates a path, with intermediate directories as needed.
+    
+    @type path: basestring
+    @param path: path to create
+    """
+    
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def _remove_path(path):
+    """
+    Removes a file or an entire directory tree.
+    
+    @type path: basestring
+    @param path: file or directory to remove
+    """
+    
+    try:
+        os.remove(path)
+    except OSError:
+        shutil.rmtree(path)
+
+
+functools.flatten = _flatten
+glob.rglob = _glob
+
+math.NaN = _NaN
+math.Infinity = _Infinity
+
+os.path.HOME = os.path.expanduser('~')
+os.path.make = _make_path
+os.path.remove = _remove_path
+
+types.BaseStringType = basestring
+types.SetType = set
+
+zipfile.ZipFile = _ZipFile
