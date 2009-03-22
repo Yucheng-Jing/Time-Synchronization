4c832773edff282571dc7a719a952704b6bad4d1
Marcio Faustino <marciof@home>
2009-03-22 20:01:11 +0000

    - Added development mode option to capture the stderr unless in production mode.

--- Web Site/index.php
+++ Web Site/index.php
@@ -1,10 +1,14 @@
 <?php
 $page = isset($_GET['page']) ? $_GET['page'] : '';
+$options = '-E UTF-8 -ansi -modern';
+$development = false;
+
 $select_page = preg_match('/^\\w+$/', $page) ? ":select $page" : '';
+$stderr = $development ? '2>&1' : '';
 
 header('X-Powered-By: Lisp');
 header('Content-Type: text/html; charset=UTF-8');
-system("clisp -E UTF-8 -ansi -modern Web.lisp $select_page", $exit_status);
+system("clisp $options Web.lisp $select_page $stderr", $exit_status);
 
 if ($exit_status <> 0) {
     include 'index.html';
