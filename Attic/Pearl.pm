9306f3cf2db27df6dae5baed49c901aa9c379650
Marcio Faustino <marciof@home>
2009-07-21 16:51:40 +0000

    - Clean up.

--- Attic/Pearl.pm
+++ Attic/Pearl.pm
@@ -1,14 +1,3 @@
-=head1 DESCRIPTION
-
-Automatically imports commonly used modules (C<English>) and turns on essential
-pragmas (C<strict>, C<utf8>, C<warnings>). It also sets some defaults and
-exports useful definitions.
-
-=head1 SYNOPSIS
-
-use Pearl;
-
-=cut
 package Pearl;
 
 use base qw(Exporter);
@@ -41,6 +30,12 @@ our @EXPORT = qw(*STDNULL $false $true async instantiate ls uncapitalize);
 our $VERSION = v2009.06.18;
 
 
+sub async(&@) {
+    tie my $result, __PACKAGE__.'::Scalar::Lazy', @ARG;
+    return \$result;
+}
+
+
 sub import {
     strict->import();
     utf8->import();
@@ -53,51 +48,6 @@ sub import {
 }
 
 
-=head1 FUNCTIONS
-
-=over 4
-
-=item async {...};
-
-=item async(FUNCTION, ARGUMENTS);
-
-Executes code asynchronously, that is, in a separate thread of execution (if
-possible). The resulting value is a reference to a scalar, which points to the
-actual result.
-
-Example:
-
-  sub greet {
-      print "Hello world!\n";
-      return 'Bye!';
-  }
-
-  my $result = async {greet()};
-
-  print "$result --> $$result\n";
-  print "Goodbye!\n";
-
-=cut
-sub async(&@) {
-    tie my $result, __PACKAGE__.'::Lazy::Scalar', @ARG;
-    return \$result;
-}
-
-
-=item instantiate(CLASS);
-
-=item instantiate(CLASS, ATTRIBUTES);
-
-Creates an instance of a class, using the given hash for the initial attributes.
-
-Example:
-
-  sub new {
-      my ($class, $name, $age) = @ARG;
-      return instantiate($class, name => $name, age => $age);
-  }
-
-=cut
 sub instantiate {
     my ($invocant, %self) = @ARG;
     my $class = ref($invocant) || $invocant;
@@ -106,18 +56,6 @@ sub instantiate {
 }
 
 
-=item ls();
-
-=item ls(DIRECTORY);
-
-Lists all entries in the given directory, or current working directory if not
-specified.
-
-Example:
-
-  ls("Documents");
-
-=cut
 sub ls {
     my ($path) = @ARG;
     $path = getcwd() unless defined $path;
@@ -130,16 +68,6 @@ sub ls {
 }
 
 
-=item uncapitalize(STRING);
-
-Removes capitalization of words.
-
-Example:
-
-  print uncapitalize("HELLO WORLD!"), "\n";
-
-=back
-=cut
 sub uncapitalize {
     my ($text) = @ARG;
     
@@ -148,38 +76,13 @@ sub uncapitalize {
 }
 
 
-=head1 FILEHANDLES
-
-=over 4
-
-=item STDNULL
-
-Standard null stream.
-
-=back
-=cut
 open STDNULL, '+<', File::Spec->devnull();
 
-=head1 CONSTANTS
+tie our $false, __PACKAGE__.'::Scalar::Constant',
+    Pearl::Scalar::Overloaded->new(0, 0, 'false');
 
-=over 4
-
-=item $false
-
-Contains boolean, number and string values for falsehood.
-=cut
-tie our $false, __PACKAGE__.'::Constant::Scalar',
-    Pearl::Overloaded::Scalar->new(0, 0, 'false');
-
-=item $true
-
-Contains boolean, number and string values for truth.
-
-=back
-=cut
-tie our $true, __PACKAGE__.'::Constant::Scalar',
-    Pearl::Overloaded::Scalar->new(1, 1, 'true');
-
+tie our $true, __PACKAGE__.'::Scalar::Constant',
+    Pearl::Scalar::Overloaded->new(1, 1, 'true');
 
 binmode STDERR, ':utf8';
 binmode STDOUT;
@@ -194,13 +97,13 @@ $WARNING = $true;
 # ------------------------------------------------------------------------------
 
 
-package Pearl::Constant::Scalar;
+package Pearl::Scalar::Constant;
 
 use strict;
 use warnings;
 
-use Carp;
-use English '-no_match_vars';
+use Carp qw(croak);
+use English qw(-no_match_vars);
 
 
 sub FETCH {
@@ -226,13 +129,13 @@ sub TIESCALAR {
 # ------------------------------------------------------------------------------
 
 
-package Pearl::Lazy::Scalar;
+package Pearl::Scalar::Lazy;
 
 use strict;
 use warnings;
 
-use Carp;
-use English '-no_match_vars';
+use Carp qw(croak);
+use English qw(-no_match_vars);
 
 
 sub FETCH {
@@ -251,14 +154,17 @@ sub STORE {
 
 
 sub TIESCALAR {
+    my ($class, $function, @arguments) = @ARG;
     my ($package) = caller;
+    
     croak('Internal package') unless $package eq Pearl::;
     
-    my ($class, $function, @arguments) = @ARG;
-    my $self = {thread => threads->create($function, @arguments)};
+    my %self = (
+        thread => threads->create($function, @arguments),
+    );
     
-    croak('Failed to create thread') unless defined $self->{thread};
-    return bless $self, $class;
+    croak('Failed to create thread') unless defined $self{thread};
+    return bless \%self, $class;
 }
 
 
@@ -270,19 +176,19 @@ sub UNTIE {
 # ------------------------------------------------------------------------------
 
 
-package Pearl::Overloaded::Scalar;
+package Pearl::Scalar::Overloaded;
 
 use strict;
 use warnings;
-
-use Carp;
-use English '-no_match_vars';
 
 use overload
     'fallback' => 1,
     'bool' => \&to_boolean,
     '0+' => \&to_number,
     '""' => \&to_string;
+
+use Carp qw(croak);
+use English qw(-no_match_vars);
 
 
 sub new {
@@ -291,13 +197,13 @@ sub new {
     
     croak('Internal package') unless $package eq Pearl::;
     
-    my $self = {
+    my %self = (
         boolean => $boolean,
         number => $number,
         string => $string,
-    };
+    );
     
-    return bless $self, $class;
+    return bless \%self, $class;
 }
 
 
@@ -319,7 +225,80 @@ sub to_string {
 }
 
 
-# ------------------------------------------------------------------------------
-
-
 1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Pearl;
+
+=head1 DESCRIPTION
+
+Automatically imports commonly used modules (C<English>), turns on essential
+pragmas (C<strict>, C<utf8>, C<warnings>), sets some defaults and exports useful
+definitions.
+
+=head1 VARIABLES
+
+=over
+
+=item C<STDNULL>
+
+Standard null stream.
+
+=item C<$false>
+
+Contains constant boolean, number and string values for falsehood.
+
+=item C<$true>
+
+Contains constant boolean, number and string values for truth.
+
+=back
+
+=head1 FUNCTIONS
+
+=head2 C<async {...} @arguments>
+
+Executes code asynchronously, that is, in a separate thread of execution (if
+possible). The resulting value is a reference to a scalar, which points to the
+actual result.
+
+    sub greet {
+        print "Hello world!\n";
+        return 'Bye!';
+    }
+
+    my $result = async {greet()};
+
+    print "$result --> $$result\n";
+    print "Goodbye!\n";
+
+=head2 C<instantiate($class, %attributes)>
+
+Creates an instance of a class, using the given hash for the initial attributes.
+
+    sub new {
+        my ($class, $name, $age) = @ARG;
+        return instantiate($class, name => $name, age => $age);
+    }
+
+=head2 C<ls($directory)>
+
+Lists all entries in the given directory, or current working directory if not
+specified.
+
+    ls("Documents");
+
+=head2 C<uncapitalize($string)>
+
+Removes capitalization of words.
+
+    print uncapitalize("HELLO WORLD!"), "\n";
+
+=head1 AUTHORS
+
+Márcio Faustino
