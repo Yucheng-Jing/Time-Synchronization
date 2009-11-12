086edc150f5cfe60a7a5c5d7709d757eea073981
Marcio Faustino <marciof@home>
2009-11-12 18:14:23 +0000

    - Suppressed "strcpy" deprecation warning under MSVC.

--- Development/Windows Mail/WindowsMail.cpp
+++ Development/Windows Mail/WindowsMail.cpp
@@ -14,6 +14,12 @@
 #include "msoeapi.h"
 
 
+#ifdef _MSC_VER
+    /* Supress "strcpy" deprecation warning. */
+    #pragma warning (disable: 4996)
+#endif
+
+
 #define PUBLIC EXTERN_C __declspec(dllexport)
 
 
