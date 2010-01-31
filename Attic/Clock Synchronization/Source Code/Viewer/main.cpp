4f6b243b5fc7399b68defcedad8b65738a7c926c
Marcio Faustino <marciof@home>
2010-01-31 18:57:51 +0000

    - Moved the window handler function to a method.

--- Attic/Clock Synchronization/Source Code/Viewer/main.cpp
+++ Attic/Clock Synchronization/Source Code/Viewer/main.cpp
@@ -26,135 +26,6 @@ namespace Win32 {
 }
 
 
-static LRESULT CALLBACK mainWindow(HWND handle, UINT message, WPARAM wParam, LPARAM lParam) {
-    static SHACTIVATEINFO shellActivate;
-    
-    switch (message) {
-    case WM_COMMAND: {
-        int menuId = LOWORD(wParam);
-        
-        switch (menuId) {
-        case IDS_SK_EXIT:
-            SendMessage(handle, WM_CLOSE, 0, 0);
-            break;
-        case IDS_SK_UPDATE:
-            break;
-        default:
-            return DefWindowProc(handle, message, wParam, lParam);
-        }
-        break;
-    }
-    case WM_CREATE:
-        memset(&shellActivate, 0, sizeof(shellActivate));
-        shellActivate.cbSize = sizeof(shellActivate);
-        
-        HINSTANCE application;
-        application = GetModuleHandle(NULL);
-        
-        HMENU menuBar;
-        menuBar = CreateMenu();
-        
-        {
-        ref<Win32::tstring> update = Win32::LoadStringT(IDS_SK_UPDATE);
-        ref<Win32::tstring> exit = Win32::LoadStringT(IDS_SK_EXIT);
-        InsertMenu(menuBar, -1, MF_BYPOSITION, IDS_SK_UPDATE, update->c_str()); 
-        InsertMenu(menuBar, -1, MF_BYPOSITION, IDS_SK_EXIT, exit->c_str());
-        }
-
-        SHMENUBARINFO menuBarInfo;
-        memset(&menuBarInfo, 0, sizeof(SHMENUBARINFO));
-        menuBarInfo.cbSize = sizeof(SHMENUBARINFO);
-        menuBarInfo.hwndParent = handle;
-        menuBarInfo.dwFlags = SHCMBF_HMENU | SHCMBF_HIDESIPBUTTON;
-        menuBarInfo.nToolBarId = (UINT) menuBar;
-        menuBarInfo.hInstRes = application;
-        
-        SHCreateMenuBar(&menuBarInfo);
-
-        CreateWindow( 
-            L"BUTTON",   // Predefined class; Unicode assumed.
-            L"OK",       // Button text. 
-            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles.
-            10,         // x position. 
-            10,         // y position. 
-            100,        // Button width.
-            100,        // Button height.
-            handle,       // Parent window.
-            NULL,       // No menu.
-            application, 
-            NULL);      // Pointer not needed.
-        
-        break;
-    case WM_PAINT: {
-        // Paint the main window:
-        PAINTSTRUCT paint;
-        HDC context = BeginPaint(handle, &paint);
-        
-        // TODO: Add any drawing code here.
-        
-        EndPaint(handle, &paint);
-        break;
-    }
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    case WM_ACTIVATE:
-        // Notify shell of our activate message.
-        SHHandleWMActivate(handle, wParam, lParam, &shellActivate, FALSE);
-        break;
-    case WM_SETTINGCHANGE:
-        SHHandleWMSettingChange(handle, wParam, lParam, &shellActivate);
-        break;
-    default:
-        return DefWindowProc(handle, message, wParam, lParam);
-    }
-    
-    return 0;
-}
-
-
-static BOOL initializeApplication(HINSTANCE application, int showMode) {
-    ref<Win32::tstring> title = Win32::LoadStringT(IDS_TITLE);
-    ref<Win32::tstring> className = Win32::LoadStringT(IDS_WINDOW_CLASS);
-    HWND window = FindWindow(className->c_str(), title->c_str());
-    
-    if (window != NULL) {
-        SetForegroundWindow(window);
-        return FALSE;
-    }
-    
-    WNDCLASS windowClass;
-    
-    windowClass.style = CS_HREDRAW | CS_VREDRAW;
-    windowClass.lpfnWndProc = mainWindow;
-    windowClass.cbClsExtra = 0;
-    windowClass.cbWndExtra = 0;
-    windowClass.hInstance = application;
-    windowClass.hIcon = NULL;
-    windowClass.hCursor = NULL;
-    windowClass.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1); //(HBRUSH) GetStockObject(WHITE_BRUSH);
-    windowClass.lpszMenuName = NULL;
-    windowClass.lpszClassName = className->c_str();
-    
-    if (RegisterClass(&windowClass) == 0) {
-        return FALSE;
-    }
-
-    window = CreateWindow(className->c_str(), title->c_str(), WS_VISIBLE,
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL,
-        application, NULL);
-    
-    if (!window) {
-        return FALSE;
-    }
-    
-    SHInitExtraControls();
-    UpdateWindow(window);
-
-    return TRUE;
-}
-
-
 class Application {
 public:
     Application() {
@@ -162,9 +33,9 @@ public:
 
 
     virtual int start(LPTSTR commandLine, int windowShowMode) {
-        MSG message;
-        
         onStart(commandLine, windowShowMode);
+        
+        MSG message;
         
         while (BOOL result = GetMessage(&message, NULL, 0, 0)) {
             if (result == -1) {
@@ -204,8 +75,8 @@ private:
     static std::map<HWND, Window*> _windows;
 
 
-    static LRESULT CALLBACK handler(HWND handle, UINT message, WPARAM wParam, LPARAM lParam) {
-        return _windows[handle]->handler(message, wParam, lParam);
+    static LRESULT CALLBACK genericHandler(HWND handle, UINT message, WPARAM wParam, LPARAM lParam) {
+        return _windows[handle]->handler(handle, message, wParam, lParam);
     }
 
 
@@ -221,7 +92,7 @@ private:
         HINSTANCE module = GetModuleHandle(NULL);
 
         windowClass.style = CS_HREDRAW | CS_VREDRAW;
-        windowClass.lpfnWndProc = &Window::handler;
+        windowClass.lpfnWndProc = Window::genericHandler;
         windowClass.cbClsExtra = 0;
         windowClass.cbWndExtra = 0;
         windowClass.hInstance = module;
@@ -276,7 +147,87 @@ public:
 
 
 private:
-    LRESULT handler(UINT message, WPARAM wParam, LPARAM lParam) {
+    LRESULT handler(HWND handle, UINT message, WPARAM wParam, LPARAM lParam) {
+        static SHACTIVATEINFO sipInfo;
+        
+        switch (message) {
+        case WM_COMMAND: {
+            int menuId = LOWORD(wParam);
+            
+            switch (menuId) {
+            case IDS_SK_EXIT:
+                SendMessage(handle, WM_CLOSE, 0, 0);
+                break;
+            case IDS_SK_UPDATE:
+                break;
+            default:
+                return DefWindowProc(handle, message, wParam, lParam);
+            }
+            break;
+        }
+        case WM_CREATE:
+            memset(&sipInfo, 0, sizeof(sipInfo));
+            sipInfo.cbSize = sizeof(sipInfo);
+            
+            HINSTANCE application;
+            application = GetModuleHandle(NULL);
+            
+            HMENU menuBar;
+            menuBar = CreateMenu();
+            
+            {
+            ref<Win32::tstring> update = Win32::LoadStringT(IDS_SK_UPDATE);
+            ref<Win32::tstring> exit = Win32::LoadStringT(IDS_SK_EXIT);
+            InsertMenu(menuBar, -1, MF_BYPOSITION, IDS_SK_UPDATE, update->c_str()); 
+            InsertMenu(menuBar, -1, MF_BYPOSITION, IDS_SK_EXIT, exit->c_str());
+            }
+
+            SHMENUBARINFO menuBarInfo;
+            memset(&menuBarInfo, 0, sizeof(SHMENUBARINFO));
+            menuBarInfo.cbSize = sizeof(SHMENUBARINFO);
+            menuBarInfo.hwndParent = handle;
+            menuBarInfo.dwFlags = SHCMBF_HMENU | SHCMBF_HIDESIPBUTTON;
+            menuBarInfo.nToolBarId = (UINT) menuBar;
+            menuBarInfo.hInstRes = application;
+            
+            SHCreateMenuBar(&menuBarInfo);
+
+            CreateWindow( 
+                L"BUTTON",   // Predefined class; Unicode assumed.
+                L"OK",       // Button text. 
+                WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles.
+                10,         // x position. 
+                10,         // y position. 
+                100,        // Button width.
+                100,        // Button height.
+                handle,       // Parent window.
+                NULL,       // No menu.
+                application, 
+                NULL);      // Pointer not needed.
+            
+            break;
+        case WM_PAINT: {
+            PAINTSTRUCT paint;
+            HDC context = BeginPaint(handle, &paint);
+            
+            // TODO: Add any drawing code here.
+            
+            EndPaint(handle, &paint);
+            break;
+        }
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        case WM_ACTIVATE:
+            SHHandleWMActivate(handle, wParam, lParam, &sipInfo, FALSE);
+            break;
+        case WM_SETTINGCHANGE:
+            SHHandleWMSettingChange(handle, wParam, lParam, &sipInfo);
+            break;
+        default:
+            return DefWindowProc(handle, message, wParam, lParam);
+        }
+        
         return 0;
     }
 
