620ec5b94b2a208dceec3c62081bd897b4315712
Marcio Faustino <marciof@home>
2010-03-27 23:25:32 +0000

    - Added CGI mode.

--- Attic/MyTVShows.pl
+++ Attic/MyTVShows.pl
@@ -1,18 +1,14 @@
 #!/usr/bin/perl
 
-# To Do:
-# - Use as CGI script.
 
-# See:
-#   http://blog.mytvshows.org/kind-of-an-api/
+use strict;
+use utf8;
+use warnings;
 
-
-# External modules:
+use CGI ();
+use English qw(-no_match_vars);
 use LWP::UserAgent ();
 use Text::xSV ();
-
-# Internal modules:
-use Pearl;
 
 
 sub alphanumerically {
@@ -20,62 +16,8 @@ sub alphanumerically {
 }
 
 
-sub get {
-    my ($url) = @ARG;
-    my $browser = LWP::UserAgent->new();
-    my $response = $browser->get("http://www.mytvshows.org/$url",
-        'User-Agent' => 'Mozilla');
-    
-    $response->is_success()
-        or die 'Download failed: '.$response->status_line()."\n";
-    
-    $response->content_type() eq 'text/html'
-        or die 'Invalid content type: '.$response->content_type()."\n";
-    
-    return $response->decoded_content();
-}
-
-
-sub get_status {
-    my ($api_key, $show, $season, $episode) = @ARG;
-    my $status = get("api/get_status/$api_key/$show/$season/$episode");
-    
-    return ($status =~ m/not\s+found/i) ? undef : $status;
-}
-
-
-sub list_episodes {
-    my ($show, $season) = @ARG;
-    my $episodes = get("show/$show/$season");
-    my @numbers = ($episodes =~ m{span\s+class="nr">([^<]+)}gi);
-    my @names = ($episodes =~ m{span\s+class="title">([^<]+)}gi);
-    
-    return map {($numbers[$ARG], $names[$ARG])} 0..$#numbers;
-}
-
-
-sub list_seasons {
-    my ($show) = @ARG;
-    my ($seasons) = (get("show/$show") =~ m{"seasons_list">.+?</ul}gis);
-    
-    return $seasons =~ m{href="/show/\Q$show\E/([^/"]+)[^>]+>([^<]+)}gi;
-}
-
-
-sub list_shows {
-    my ($user_name) = @ARG;
-    return get("user/$user_name") =~ m{href="/show/([^/"]+)[^>]+>([^<]+)}gi;
-}
-
-
-sub main {
-    unless ((@ARGV > 0) && (@ARGV < 3)) {
-        print <<'USAGE' and return;
-Usage: <user name> [API key]
-USAGE
-    }
-    
-    my ($user_name, $api_key) = @ARGV;
+sub generate {
+    my ($user_name, $api_key) = @ARG;
     my %shows = list_shows($user_name);
     my $csv = Text::xSV->new();
     my @header = ('Name ID', 'Name');
@@ -111,6 +53,76 @@ USAGE
     else {
         $csv->print_row($ARG => $shows{$ARG}) foreach sort keys %shows;
     }
+}
+
+
+sub get {
+    my ($url) = @ARG;
+    my $browser = LWP::UserAgent->new();
+    my $response = $browser->get("http://www.mytvshows.org/$url",
+        'User-Agent' => 'Mozilla');
+    
+    $response->is_success()
+        or die 'Download failed: '.$response->status_line()."\n";
+    
+    $response->content_type() eq 'text/html'
+        or die 'Invalid content type: '.$response->content_type()."\n";
+    
+    return $response->decoded_content();
+}
+
+
+# See: http://blog.mytvshows.org/kind-of-an-api/
+sub get_status {
+    my ($api_key, $show, $season, $episode) = @ARG;
+    my $status = get("api/get_status/$api_key/$show/$season/$episode");
+    
+    return ($status =~ m/not\s+found/i) ? undef : $status;
+}
+
+
+sub list_episodes {
+    my ($show, $season) = @ARG;
+    my $episodes = get("show/$show/$season");
+    my @numbers = ($episodes =~ m{span\s+class="nr">([^<]+)}gi);
+    my @names = ($episodes =~ m{span\s+class="title">([^<]+)}gi);
+    
+    return map {($numbers[$ARG], $names[$ARG])} 0..$#numbers;
+}
+
+
+sub list_seasons {
+    my ($show) = @ARG;
+    my ($seasons) = (get("show/$show") =~ m{"seasons_list">.+?</ul}gis);
+    
+    return $seasons =~ m{href="/show/\Q$show\E/([^/"]+)[^>]+>([^<]+)}gi;
+}
+
+
+sub list_shows {
+    my ($user_name) = @ARG;
+    return get("user/$user_name") =~ m{href="/show/([^/"]+)[^>]+>([^<]+)}gi;
+}
+
+
+sub main {
+    my ($user_name, $api_key) = (CGI::url_param('user'), CGI::url_param('key'));
+    
+    if (defined $user_name) {
+        autoflush STDOUT;
+        binmode STDOUT, ':utf8';
+        print "Content-type: text/plain; charset=UTF-8\n\n";
+    }
+    else {
+        unless ((@ARGV > 0) && (@ARGV < 3)) {
+            print "Usage: <user name> [API key]\n";
+            return;
+        }
+        
+        ($user_name, $api_key) = @ARGV;
+    }
+    
+    generate($user_name, $api_key);
 }
 
 
