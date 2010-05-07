d302e75c6cab361134172efe29a62c55357fa5ec
Marcio Faustino <marciof@home>
2010-05-07 15:17:19 +0000

    - Added core header file and library references.

--- Attic/Time Synchronization/Code/Library/wm/Object.h
+++ Attic/Time Synchronization/Code/Library/wm/Object.h
@@ -8,7 +8,11 @@
 // Order is not significant:
 #include <COMMON/SDK/INC/service.h>
 #include <cpl.h>
+#include <projects.h>
 #include "core/ref.h"
+
+
+#pragma comment(lib, "note_prj.lib")
 
 
 // Private kernel functions.
