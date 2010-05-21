31bbf6cbb1a593ea8c67ff4b7dddc13b65e1dbbe
Marcio Faustino <marciof@home>
2010-05-21 10:33:33 +0000

    - Removed Coming Soon and MyTVShows User JS's.

--- Attic/Opera User JS/MyTVShows.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @include http://www.mytvshows.org/mark/all/seen/*
-// ==/UserScript==
-
-
-var showName = location.href.match(/seen\/([^\/]+)/).pop();
-window.stop();
-location = 'http://www.mytvshows.org/show/' + showName + '/';
