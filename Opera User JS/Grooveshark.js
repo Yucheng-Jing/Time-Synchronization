c23394a0c3ade5b791be100cb466d52127892b9a
Marcio Faustino <marciof@home>
2010-07-10 14:25:19 +0000

    - Fixed Grooveshark User JS.

--- Opera User JS/Grooveshark.js
+++ Opera User JS/Grooveshark.js
@@ -1,12 +1,15 @@
 // ==UserScript==
-// @include http://listen.grooveshark.com/
+// @include http://listen.grooveshark.com/*
 // ==/UserScript==
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    var adBar = document.getElementById('adBar');
-    var mainContentWrapper = document.getElementById('mainContentWrapper');
+    function $(/* ... */) {
+        return document.getElementById.apply(document, arguments);
+    }
     
-    adBar.parentNode.removeChild(adBar);
-    mainContentWrapper.style.width = '100%';
+    var ad = $('adBar') || $('sidebar');
+    
+    ad.parentNode.removeChild(ad);
+    $('mainContentWrapper').style.width = '100%';
 }, false);
