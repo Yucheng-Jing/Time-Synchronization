c4999916b45e078f23ed58627183b76d817ec3c7
Marcio Faustino <marciof@home>
2011-04-25 15:31:05 +0200

    Added draft control panel applet code.

---
+++ draft/applet/Applet.cpp
@@ -0,0 +1,15 @@
+#include <wm/Log.h>
+
+
+extern "C"
+BOOL APIENTRY DllMain(HANDLE module, DWORD mode, LPVOID reserved) {
+    wm::Log::error(TEXT("##### DLL\n"));
+    return TRUE;
+}
+
+
+extern "C" __declspec(dllexport) 
+LONG APIENTRY CPlApplet(HWND window, UINT message, LONG param1, LONG param2) {
+    wm::Log::error(TEXT("##### CPL\n"));
+    return 1;
+}
